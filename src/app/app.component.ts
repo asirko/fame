13ed5a69f054678f2c179d93a72d3cf86f86a75a
asirko <alexandre@jump-biz.com>
2018-06-11 18:32:21 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fame-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fame';
+}
