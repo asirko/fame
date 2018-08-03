a01ae6af29d08fe6c500b52dda0c348d4209b755
asirko <alexandre@jump-biz.com>
2018-08-03 19:00:38 +0200

    Ajout de l'état "le quiz n'a pas démarrer" + gestion de flag observable dans admin service

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -2,38 +2,43 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
 import * as SocketIOClient from 'socket.io-client';
 import {GameState, Question} from '../question';
-import {skip} from 'rxjs/operators';
+import {map, skip} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
   private _socket = SocketIOClient('/game');
-  private _currentQuestion$ = new BehaviorSubject<Question>(null);
+  private _currentQuestion$ = new BehaviorSubject<Question | GameState>(null);
 
   // the first is skipped because behaviorSubject have an initial value;
-  readonly currentQuestion$ = this._currentQuestion$.asObservable().pipe(skip(1));
+  readonly hasNotStarted$: Observable<boolean>;
+  readonly currentQuestion$: Observable<Question>;
+  readonly hasFinished$: Observable<boolean>;
 
   constructor() {
-    this.initCurrentQuestion();
-  }
-
-  /**
-   * get the current question of the game
-   * null -> not started
-   * undefined -> end of the quiz
-   * @returns {Observable<any[]>}
-   */
-  initCurrentQuestion(): void {
+    /**
+     * get the current question of the game
+     * before to start and after it ends its hold a GameState
+     * and a question otherwise
+     * those values are cleaned up for the public API
+     */
     this._socket.on('currentQuestion', question => {
-      if (question === GameState.NOT_STARTED) {
-        this._currentQuestion$.next(null);
-      } else if (question === GameState.FINISHED) {
-        this._currentQuestion$.next(undefined);
-      } else {
-        this._currentQuestion$.next(question);
-      }
+      this._currentQuestion$.next(question);
     });
+
+    this.hasNotStarted$ = this._currentQuestion$.asObservable().pipe(
+      skip(1),
+      map(q => q === GameState.NOT_STARTED),
+    );
+    this.currentQuestion$ = this._currentQuestion$.asObservable().pipe(
+      skip(1),
+      map(q => q === GameState.NOT_STARTED || q === GameState.FINISHED ? null : q),
+    );
+    this.hasFinished$ = this._currentQuestion$.asObservable().pipe(
+      skip(1),
+      map(q => q === GameState.FINISHED),
+    );
   }
 
   nextQuestion(): Observable<void> {
