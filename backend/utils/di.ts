342e731aad656bb74d9ab31bffca6cf7726bbc74
asirko <alexandre@jump-biz.com>
2018-08-16 07:59:33 +0200

    Ajout de commentaires

--- backend/utils/di.ts
+++ backend/utils/di.ts
@@ -11,13 +11,25 @@ interface Type<T> {
 
 type GenericClassDecorator<T> = (target: T) => void;
 
+/**
+ * Permet l'injection de dépendence par type dans un le constructeur de la classe décoré
+ * Si une class n'est pas décorée, elle n'a pas de meta données
+ */
 export const Service = (): GenericClassDecorator<Type<object>> => {
   return (target: Type<object>) => {};
 };
+
+/**
+ * Permet l'injection de dépendence par type dans un le constructeur de la classe décoré
+ * Si une class n'est pas décorée, elle n'a pas de meta données
+ */
 export const Api = (): GenericClassDecorator<Type<object>> => {
   return (target: Type<object>) => {};
 };
 
+/**
+ * Permet d'injecter manuellement une dépendence
+ */
 export const Injector = new class {
   // resolving instances
   resolve<T>(target: Type<any>): T {
