c30b1cd601b33265d8b8b072beaa105f5ddb2cc2
asirko <sirko.alexandre@gmail.com>
2018-10-08 14:00:48 +0200

    test d'accès avec concurrence

--- scripts/onLoadStats.ts
+++ scripts/onLoadStats.ts
@@ -3,29 +3,42 @@ import { promisify } from 'util';
 import { appendFileSync, writeFile } from 'fs';
 
 const fileName = 'scripts/logs.csv';
+const url = 'http://game.biz';
 
-async function test(nb_try = 10) {
-  const browser = await launch({headless: true});
-  for (let i = 0; i < nb_try; i++) {
-    console.log('tentative démarée à ' + new Date());
-    const ms = await singleTest(browser);
-    appendFileSync(fileName, `${i},${ms}\n`);
+async function test(nbTry = 10) {
+  const browser = await launch({headless: false});
+  for (let i = 1; i < 50; i++) {
+    console.log(`Avec ${i} appels concurrent`);
+    await concurrentTest(browser, i, nbTry);
+    appendFileSync(fileName, `\n`);
   }
   await browser.close();
 }
 
+async function concurrentTest(browser: Browser, nbConcurrent: number, nbTry = 10) {
+  for (let i = 0; i < nbTry; i++) {
+    console.log('\ttentative démarée à ' + new Date());
+    const allTries: Promise<number>[] = [];
+    for (let j = 0; j < nbConcurrent; j++) {
+      allTries.push(singleTest(browser));
+    }
+    const ms = await Promise.all(allTries);
+    appendFileSync(fileName, `${i},${ms.join((','))}\n`);
+  }
+}
+
 async function singleTest(browser: Browser): Promise<number> {
-  const msStart = new Date().getTime();
 
   const page = await browser.newPage();
-  await page.goto('https://anim-trader.firebaseapp.com/catalog', {waitUntil: 'networkidle2'});
+  const msStart = new Date().getTime();
+  await page.goto(url, {waitUntil: 'networkidle2'});
+  const msEnd = new Date().getTime();
   await page.close();
 
-  const msEnd = new Date().getTime();
   return msEnd - msStart;
 }
 
-promisify(writeFile)(fileName, 'iter,durée\n')
+promisify(writeFile)(fileName, 'iter,durées\n')
   .then(() => test(process.argv[2] && +process.argv[2]))
   .then(() => console.log('done'))
   .catch(error => console.log(error));
